4d92fdcad8a7550b3256a37cf167a553419f2f8b
Alberto Saz <albertosaz@gmail.com>
2024-01-11 13:21:57 +0100

    event

--- pikachu/force-app/main/default/aura/nombre/nombreController.js
+++ pikachu/force-app/main/default/aura/nombre/nombreController.js
@@ -3,15 +3,16 @@
 
     }, 
     saludar : function(component, event, helper) {
-        alert("antes eras " + component.get("v.nombre") + " y ahora eres Alert!!!");
-        component.set("v.nombre", "Alert!!!")
+        alert("antes eras " + component.get("v.nombreOld") + " y ahora eres "+ component.get("v.nombre"));
         //trigers an event
-        var evt = $A.get("e.c:evtData");
-        var obj = {
+        var evt = $A.get("e.c:evtData"); //cojo el evento que existe
+        var obj = {//creo un objeto con los datos que quiero pasar
             "nombre" : component.get("v.nombre"),
             "apellido" : "Alert!!!"
         };
-        evt.setParams(obj);
-        evt.fire();
+
+        evt.setParams(obj); // cargo los datos en el evento
+        component.set("v.nombreOld", component.get("v.nombre"))
+        evt.fire(); //enviar el evento para que lo escuche alguien
     }
 })
